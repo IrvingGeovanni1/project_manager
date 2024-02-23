56968230ae08720de8e22393975389f027bb807a
Misael <leasim3x@gmail.com>
2024-02-23 01:48:11 -0600

    Creación del modelo user personalizado

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    id = models.CharField(primary_key=True, blank=True)
+    type_user = models.CharField(max_lenght=50)
+    update_at = models.DateTimeField(auto_now=True)
