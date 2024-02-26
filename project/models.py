e0d123d0dc7e3bc0d70cd75a2fd6dfe78230c8d7
Misael <leasim3x@gmail.com>
2024-02-26 09:03:07 -0600

    Implementación de Modelos Poyecto y Tarea

--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    name_project = models.CharField(max_lenght=100)
+    category = models.CharField(max_lenght=50)
+    complete = models.BooleanField(default=False)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
--- task/models.py
+++ task/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from project.models import Project
 
 # Create your models here.
+
+class Task(models.Model):
+    id_task = models.AutoField(primary_key=True)
+    title = models.CharField(max_field=100)
+    descripcion = models.CharField(max_length=500)
+    complete = models.BooleanField(default=False)
+    id_projectr = models.ForeignKey(Project,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
