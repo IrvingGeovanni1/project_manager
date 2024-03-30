3e298e88a06b9be5b707cd97800ba9a0ac03036c
Misael <leasim3x@gmail.com>
2024-03-30 05:39:09 -0600

    Migración a MySQL

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+# Import Enviroment variables
+from dotenv import load_dotenv
+
+# Loading enviroment variables from .env
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,8 +91,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'), # Or the IP of the database server if remote
+        'PORT': os.getenv('DB_PORT',  '3306'), # The default port for MySQL
+        'OPTIONS': { # When Django establishes a new connection, which will activate Strict Mode.
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
